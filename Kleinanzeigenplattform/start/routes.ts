9ebb401d489c80bf4b5928c937fa2f6d765051bf
Sven Sendke <svensendke@gmail.com>
2024-03-22 17:16:37 +0100

    feat: added .edge files for the elements in the navbar

--- Kleinanzeigenplattform/start/routes.ts
+++ Kleinanzeigenplattform/start/routes.ts
@@ -9,4 +9,22 @@
 
 import router from '@adonisjs/core/services/router'
 
-router.on('/').render('pages/home')
+router.get('/', async ({ response }) => {
+  return response.redirect('/home')
+})
+
+router.get('/home', async ({ view }) => {
+  return view.render('pages/home')
+})
+
+router.get('/home/kleine_preise', async ({ view }) => {
+  return view.render('pages/k-preise')
+})
+
+router.get('/home/hilfe', async ({ view }) => {
+  return view.render('pages/hilfe')
+})
+
+router.get('/home/anzeige_aufgeben', async ({ view }) => {
+  return view.render('pages/anzeige-aufgeben')
+})
