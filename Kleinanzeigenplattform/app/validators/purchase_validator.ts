dcbf0fc9003715af8e0c68c9753912033b8e2590
Sven Sendke <svensendke@gmail.com>
2024-05-11 15:30:06 +0200

    refactor: free items now work

--- Kleinanzeigenplattform/app/validators/purchase_validator.ts
+++ Kleinanzeigenplattform/app/validators/purchase_validator.ts
@@ -7,6 +7,6 @@ export const creditCardValidator = vine.compile(
     cardNumber: vine.string().trim().creditCard(),
     expirationDate: vine.string().trim().minLength(5).maxLength(5).regex(regexDate),
     cvv: vine.string().trim().minLength(3).maxLength(3),
-    price: vine.number().min(0).max(9999999999.99).decimal(2).positive()
+    price: vine.number().min(0).max(9999999999.99).decimal([0, 2]).positive()
   })
 )
