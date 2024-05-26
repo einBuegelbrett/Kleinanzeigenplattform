a76a82d02d228ec3e43248b3e0492012f511e274
Sven Sendke <svensendke@gmail.com>
2024-05-26 15:49:43 +0200

    fix: repaired search field

--- Kleinanzeigenplattform/app/controllers/home_controller.ts
+++ Kleinanzeigenplattform/app/controllers/home_controller.ts
@@ -21,11 +21,11 @@ export default class HomeController {
     await auth.check()
     const search = await request.validateUsing(searchbar)
     try {
-      if(search === null) {
+      if(search.search === null) {
         const items = await db.from('items').select('*').innerJoin('images', 'items.item_id', 'images.item_id').groupBy('items.item_id').where('items.active', 1).limit(12);
         return view.render('pages/home/home', { user: session.get('user'), items });
       } else {
-        const items = await db.from('items').select('*').innerJoin('images', 'items.item_id', 'images.item_id').groupBy('items.item_id').where('items.title', 'like', `%${search}%`).andWhere('items.active', 1).limit(12);
+        const items = await db.from('items').select('*').innerJoin('images', 'items.item_id', 'images.item_id').groupBy('items.item_id').where('items.title', 'like', `%${search.search}%`).andWhere('items.active', 1).limit(12);
         return view.render('pages/home/home', { user: session.get('user'), items, search: search.search });
     }} catch (error) {
       return view.render('pages/home/home', { error: 'Fehler bei der Suche'});
